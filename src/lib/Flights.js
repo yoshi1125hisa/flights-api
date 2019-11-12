e542e201b5092f0d1caf1f05bec33867a2b8fc9e
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-11-12 10:01:59 +0900

    feat(coding): fix func-name flights

--- src/lib/Flights.js
+++ src/lib/Flights.js
@@ -2,21 +2,29 @@ const Request = require('./FetchWrap');
 const express = require('./Express');
 
 const request = new Request('https://api-tokyochallenge.odpt.org/api/v4');
-let departureFlights = []
+let flights = []
 
-const getDeparture = async () => {
+const getFlights = async () => {
     try {
         let flightData = await request.get('/odpt:FlightSchedule?odpt:operator=odpt.Operator:HND-JAT');
         // console.log(flightData);
         for (let i = 0; i < flightData.length; i++) {
             // 発
             let originAirport = flightData[i]['odpt:originAirport'].slice(13);
+            let destinationAirport = flightData[i]['odpt:destinationAirport'].slice(13);
             // 羽田発便のみ
-            if (originAirport != "HND") {
+            if (originAirport != "HND" && destinationAirport != "HND") {
                 continue;
             }
-            let destinationAirport = flightData[i]['odpt:destinationAirport'].slice(13);
-            if ((destinationAirport).indexOf('OBO') >= 0 ||
+            if ((originAirport).indexOf('OBO') >= 0 ||
+                (originAirport).indexOf('KMQ') >= 0 ||
+                (originAirport).indexOf('KIX') >= 0 ||
+                (originAirport).indexOf('IZO') >= 0 ||
+                (originAirport).indexOf('TKS') >= 0 ||
+                (originAirport).indexOf('HIJ') >= 0 ||
+                (originAirport).indexOf('KMJ') >= 0 ||
+                (originAirport).indexOf('OKA') >= 0 ||
+                (destinationAirport).indexOf('OBO') >= 0 ||
                 (destinationAirport).indexOf('KMQ') >= 0 ||
                 (destinationAirport).indexOf('KIX') >= 0 ||
                 (destinationAirport).indexOf('IZO') >= 0 ||
@@ -29,22 +37,22 @@ const getDeparture = async () => {
                 for (let j = 0; j < flightScheduleObj.length; j++) {
                     let originTime = flightScheduleObj[j]['odpt:originTime'];
                     let destinationTime = flightScheduleObj[j]['odpt:destinationTime'];
-                    departureFlights.push({
+                    flights.push({
                         "origin_airport": originAirport,
                         "destination_airport": destinationAirport,
                         "origin_time": originTime,
                         "destination_time": destinationTime
                     })
-                    // console.log(departureFlights)
+                    // console.log(flights)
                 }
             }
 
             // console.log(originAirport + ' → ' + destinationAirport);
             // console.log(flightScheduleObj)
         }
-        // console.log(departureFlights);
-        express(departureFlights);
-        // return departureFlights;
+        // console.log(flights);
+        express(flights);
+        // return flights;
     } catch (e) {
         console.log('取得できませんでした');
         console.log(e);
@@ -52,4 +60,4 @@ const getDeparture = async () => {
     }
 }
 
-getDeparture();
+getFlights();
--- src/lib/express.js
+++ src/lib/express.js
@@ -14,13 +14,11 @@ module.exports = requests => {
     app.get('/', (req, res) =>
         res.send('pong')
     );
-    app.get('/v1/departure', (req, res) =>
+    app.get('/v1/flights', (req, res) =>
         res.send(requests),
     );
-    app.get('/v1/arrival', (req, res) =>
-        res.send(requests)
-    );
-    app.listen(app.get('port'), function () {
-        console.log("Node app is running at localhost:" + app.get('port'))
-    });
 }
+
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'))
+});
