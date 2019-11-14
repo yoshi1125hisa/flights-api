f64134d293c601ba0cdf77a3f6f355b33108e833
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-11-14 11:35:24 +0900

    🤩(coding): add favicon

--- src/lib/Express.js
+++ src/lib/Express.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const cors = require('cors')
 const app = express();
+const favicon = require('serve-favicon');
+const path = require('path');
+
 app.set('port', (process.env.PORT || 3000));
-app.use(cors())
+app.use(cors());
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 module.exports = requests => {
     app.get('/', (req, res) =>
-        res.send('pong')
+        res.send('This API is working!')
     );
     app.get('/v1/flights', (req, res) =>
         res.send(requests),
