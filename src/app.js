3ade70086cf4c18709c59ab234d5a78050b8d0ea
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-11-14 23:36:24 +0900

    🤩(coding): dateを比較し、当日のものおよび重複なしのもののみ取得

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const Request = require('./lib/FetchWrap');
 const express = require('./lib/Express');
+const Moment = require('moment');
+require('moment-timezone');
+const MomentRange = require('moment-range');
+
+const moment = MomentRange.extendMoment(Moment);
+moment.locale('ja');
+moment.tz.setDefault('Asia/Tokyo');
 
 const request = new Request('https://api-tokyochallenge.odpt.org/api/v4');
-let flights = []
+let flights = [];
 
 const getFlights = async () => {
     try {
         let flightData = await request.get('/odpt:FlightSchedule?odpt:operator=odpt.Operator:HND-JAT');
-        // console.log(flightData);
         for (let i = 0; i < flightData.length; i++) {
             // 発
             let originAirport = flightData[i]['odpt:originAirport'].slice(13);
@@ -35,28 +41,34 @@ const getFlights = async () => {
             ) {
                 let flightScheduleObj = flightData[i]['odpt:flightScheduleObject']
                 for (let j = 0; j < flightScheduleObj.length; j++) {
+                    let vaildFrom = flightScheduleObj[j]['odpt:isValidFrom'];
+                    let vaildTo = flightScheduleObj[j]['odpt:isValidTo'];
+                    let vaildRange = moment.rangeFromISOString(vaildFrom + '/' + vaildTo);
+                    let isVaild = vaildRange.contains(moment()); // trueなら範囲内
                     let airline = flightScheduleObj[j]['odpt:airline'].slice(14);
                     let flightNum = flightScheduleObj[j]['odpt:flightNumber'][0];
                     let originTime = flightScheduleObj[j]['odpt:originTime'];
                     let destinationTime = flightScheduleObj[j]['odpt:destinationTime'];
-                    flights.push({
-                        "airline": airline,
-                        "flight_number": flightNum,
-                        "origin_airport": originAirport,
-                        "destination_airport": destinationAirport,
-                        "origin_time": originTime,
-                        "destination_time": destinationTime
-                    })
-                    // console.log(flights)
+
+                    if(isVaild === true){
+                        flights.push({
+                            "airline": airline,
+                            "flight_number": flightNum,
+                            "origin_airport": originAirport,
+                            "destination_airport": destinationAirport,
+                            "origin_time": originTime,
+                            "destination_time": destinationTime
+                        })
+                    }
                 }
             }
-
-            // console.log(originAirport + ' → ' + destinationAirport);
-            // console.log(flightScheduleObj)
         }
-        // console.log(flights);
-        express(flights);
-        // return flights;
+        var cleanFlights = flights.filter(function (v1, i1, a1) {
+            return (a1.findIndex(function (v2) {
+                return (v1.flight_number === v2.flight_number)
+            }) === i1);
+        });
+        express(cleanFlights);
     } catch (e) {
         console.log('取得できませんでした');
         console.log(e);
